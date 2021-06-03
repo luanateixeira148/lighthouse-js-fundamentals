0b3221f6595397ba19d368aee439a85512282ca5
luanateixeira148 <luanateixeira148@gmail.com>
2021-06-03 11:45:52 -0700

    created a function to check if a number is even

---
+++ even.js
@@ -0,0 +1,11 @@
+//is this the same as isEven(num)? what is the advantage of declaring an annonymous function as a variable?
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+console.log(tenIsEven);
+
+//or, in a simplified way:
+
+consele.log(isEven(11));
