36973abf01db66837e166a40ab29a99a791d0017
luanateixeira148 <luanateixeira148@gmail.com>
2021-06-04 21:59:18 +0000

    looping over array

---
+++ packing-list.js
@@ -0,0 +1,5 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+for (let i = o; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
