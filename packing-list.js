09830df77014599223ed9182e3578568269bc969
luanateixeira148 <luanateixeira148@gmail.com>
2021-06-04 22:02:24 +0000

    looping over array with While

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,14 @@
+// const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+// for (let i = o; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-for (let i = o; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
